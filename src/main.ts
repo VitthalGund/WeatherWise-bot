3eef30900f1f759e45b37dadf362a68697858862
VitthalGund <vitthalgund30@gmail.com>
2024-01-01 12:26:04 +0530

    origin policies updated

--- src/main.ts
+++ src/main.ts
@@ -5,23 +5,23 @@ import { NextFunction, Response } from 'express';
 const allowedOrigins = ['http://localhost:5173', process.env.origin];
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  if (process.env.APP_ENV !== 'production') {
-    app.enableCors({
-      origin: process.env.origin,
-      credentials: true,
-    });
-  } else {
-    app.enableCors({
-      origin: 'http://localhost:5173',
-      credentials: true,
-    });
-  }
+  // if (process.env.APP_ENV !== 'production') {
+  //   app.enableCors({
+  //     origin: process.env.origin,
+  //     credentials: true,
+  //   });
+  // } else {
+  //   app.enableCors({
+  //     origin: 'http://localhost:5173',
+  //     credentials: true,
+  //   });
+  // }
 
   app.use(function (request: Request, response: Response, next: NextFunction) {
     const origin = request.headers['origin'];
     if (allowedOrigins.includes(origin)) {
       // console.log(origin);
-      response.header('Access-Control-Allow-Origin', origin);
+      response.header('Access-Control-Allow-Origin', 'true');
     }
     next();
   });
